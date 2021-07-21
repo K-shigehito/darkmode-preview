1aaac08956bb0f9302ff1355e77da021183a0258
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-07-22 01:08:43 +0900

    sample-02

--- sample-02/main.js
+++ sample-02/main.js
@@ -7,10 +7,12 @@ const modeSwitch = document.getElementById("myonoffswitch");
 // ダークモードがオンの時に実行する処理
 function darkModeOn() {
   document.documentElement.classList.add("darkmode"); // ルート要素<html>にクラスを追加
+  modeSwitch.checked = true;
 }
 // ダークモードがオフの時に実行する処理
 function darkModeOff() {
   document.documentElement.classList.remove("darkmode"); // クラスの削除
+  modeSwitch.checked = false;
 }
 
 // イベントリスナー
@@ -21,18 +23,25 @@ const listener = (event) => {
     darkModeOff();
   }
 };
-
 // リスナー登録
 osDark.addEventListener("change", listener);
-
 // 初期化処理
 listener(osDark);
 
 // スイッチの操作に応じて切り替え処理
 modeSwitch.addEventListener("change", () => {
   if (modeSwitch.checked) {
-    darkModeOff();
-  } else {
     darkModeOn();
+    sessionStorage.setItem("darkMode", "on");
+  } else {
+    darkModeOff();
+    sessionStorage.setItem("darkMode", "off");
   }
 });
+
+// ロード時の状況に応じて切り替え
+if (sessionStorage.getItem("darkMode") === "on") {
+  darkModeOn();
+} else if (sessionStorage.getItem("darkMode") === "off") {
+  darkModeOff();
+}
