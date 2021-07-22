d3fd0a7b27c777e8156e22ef72b843d7ed60148f
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-07-22 11:55:46 +0900

    sample-03

---
+++ sample-03/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./dist/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
